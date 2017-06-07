28d60507767b3a392761c6bdfd4c119b013632df
Tim <tjcutt@gmail.com>
2017-06-07 14:25:21 -0600

    Sadly fell down the rabbit hole and did not commit throughout the process

---
+++ server/migrations/20170517213859_classifieds.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('classifieds', table => {
+    table.increments();
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('price').notNullable();
+    table.string('item_image').notNullable();
+    table.dateTime("created_at").notNullable().defaultTo(knex.fn.now());
+    table.dateTime("updated_at").notNullable().defaultTo(knex.fn.now());
+    // table.timestamps(true, true);
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+return knex.schema.dropTable('classifieds');
+};
